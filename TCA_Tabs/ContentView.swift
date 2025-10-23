87c77b5c8305d6afafc09e9aebd97a751e23f892
Dmitriy <dimkoy01@gmail.com>
2025-10-23 14:23:08 +0200

    Initial Commit

---
+++ TCA_Tabs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Tabs
+//
+//  Created by Dima on 23/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Tabs/TCA_TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TabsApp.swift
+//  TCA_Tabs
+//
+//  Created by Dima on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
